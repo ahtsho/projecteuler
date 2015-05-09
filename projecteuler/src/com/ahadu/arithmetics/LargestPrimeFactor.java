bf50c0def18b6eb32524d5e737f4de288ef94986
ahtsho <tsegaye.ahadu@gmail.com>
2015-05-09 10:38:47 +0200

    created main meghod for LargestPrimeFactor

--- projecteuler/src/com/ahadu/arithmetics/LargestPrimeFactor.java
+++ projecteuler/src/com/ahadu/arithmetics/LargestPrimeFactor.java
@@ -10,6 +10,10 @@ public class LargestPrimeFactor {
 	 * The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime
 	 * factor of the number 600.851.475.143 ?
 	 */
+	
+	public static void main (String[] args){
+		System.out.println(calculateLargestPrimeFactorOf(600851475143.0));
+	}
 	public static double calculateLargestPrimeFactorOf(double n) {
 
 		ArrayList<Double> factors = findPrimeDivisors(n);
